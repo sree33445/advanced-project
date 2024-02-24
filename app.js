5b165f89a6128326e7a1184e78bb6ab295c3e7a8
sree33445 <116620904+sree33445@users.noreply.github.com>
2024-02-24 15:58:31 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Starter from './component/Users';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Starter/>
+    </div>
+  );
+}
+
+export default App;
