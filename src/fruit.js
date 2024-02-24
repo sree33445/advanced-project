3cf7df1c76bb1dc0273616bab725e5f5ca887e81
sree33445 <116620904+sree33445@users.noreply.github.com>
2024-02-24 16:04:01 +0530

    Create fruit.js

---
+++ src/fruit.js
@@ -0,0 +1,40 @@
+
+const fruits = [
+    {
+      id: 1,
+      fruitName: "Strawberry",
+      price: 125,
+      image:
+        "https://images.unsplash.com/photo-1601004890684-d8cbf643f5f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZnJ1aXR8ZW58MHx8MHx8&w=1000&q=80",
+    },
+    {
+      id: 2,
+      fruitName: "Pear",
+      price: 230,
+      image: "https://ume.co.in/wp-content/uploads/2022/01/umekart_Pear.jpg",
+    },
+    {
+      id: 3,
+      fruitName: "Cherry",
+      price: 250,
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Cherry_season_%2848216568227%29.jpg/640px-Cherry_season_%2848216568227%29.jpg",
+    },
+    {
+      id: 4,
+      fruitName: "Cherry",
+      price: 250,
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Cherry_season_%2848216568227%29.jpg/640px-Cherry_season_%2848216568227%29.jpg",
+    },
+    {
+      id: 5,
+      fruitName: "Kiwi",
+      price: 175,
+      image:
+        "https://i1.wp.com/pixahive.com/wp-content/uploads/2020/12/Kiwi-fruit-254550-pixahive.jpg?fit=2560%2C1707&ssl=1",
+    },
+  ];
+  
+  export default fruits;
+  
